7a4c314ac3b5e7f923f053e1f108820d3f334a2e
nishantnepal3 <chocowafers4@gmail.com>
2024-11-10 21:37:47 -0500

    added jest project and created jest.config.js file

---
+++ JEST-DEMO/jest.config.js
@@ -0,0 +1,9 @@
+﻿module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  }
+};
